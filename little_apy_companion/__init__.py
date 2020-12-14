918a193d8e61d158da423642872b11548a31ebe7
Caity-chan <74330895+Caity-chan@users.noreply.github.com>
2020-12-13 21:15:47 -0800

    added typing effect option

--- little_apy_companion/__init__.py
+++ little_apy_companion/__init__.py
@@ -2,7 +2,7 @@ import os
 import discord
 from importlib import reload
 from importlib import import_module
-async def handle(execute, prefix, message, client, owner_ids):
+async def handle(execute, prefix, message, client):
 	if message.content.startswith(prefix):
 		command_and_args = message.content[len(prefix):]
 		command_and_args = command_and_args.split(" ")
@@ -11,8 +11,10 @@ async def handle(execute, prefix, message, client, owner_ids):
 		if command != "reload":
 			for cmd in execute:
 				if cmd.cmd == command:
+					if cmd.typing == True:
+						await message.channel.trigger_typing()
 					if cmd.executable_by == "bot_owner":
-						if message.author.id in owner_ids:
+						if message.author.id == 563980783828860944:
 							await cmd.execute(command, args, message, client)
 						else:
 							await message.channel.send("You are not the bot owner!")
@@ -26,7 +28,8 @@ async def handle(execute, prefix, message, client, owner_ids):
 						else:
 							await cmd.execute(command, args, message, client)
 		else:
-			if message.author.id in owner_ids:
+			await message.channel.trigger_typing()
+			if message.author.id == 563980783828860944:
 				execute = []
 				for filename in os.listdir('/home/container/commands'):
 					if filename.endswith('.py'):
